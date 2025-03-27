e532eff0bea9ebf9d6e6e6014285e8cd063ced3f
yst314 <38710830+yst314@users.noreply.github.com>
2025-03-27 10:49:28 +0900

    Update sp500.py

--- sp500.py
+++ sp500.py
@@ -12,15 +12,26 @@ def get_sp500_close():
         "range": "2d",
         "interval": "1d"
     }
-    res = requests.get(url, params=params).json()
-    closes = res["chart"]["result"][0]["indicators"]["quote"][0]["close"]
-    last_close = closes[-2]  # 前日終値（-1は今日）
-    return round(last_close, 2)
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    res = requests.get(url, params=params, headers=headers)
 
+    if res.status_code != 200:
+        raise Exception(f"Yahoo Finance API error: {res.status_code}")
+
+    try:
+        closes = res.json()["chart"]["result"][0]["indicators"]["quote"][0]["close"]
+        last_close = closes[-2]  # 前日終値（-1は今日）
+        return round(last_close, 2)
+    except Exception as e:
+        print("レスポンス内容:", res.text)
+        raise e
 def send_to_discord(price):
     text = f"📈 S&P500 前日終値: ${price}"
     data = {
         "username": "ナカヤマ",
+        "avatar_url": "https://nagauchi.notion.site/image/attachment%3Ab902c629-b9e6-4a80-ad6e-8e3fede730f7%3Aimage.png?table=block&id=1c2b7378-9dfa-8080-89e8-f7277514877b&spaceId=a484c95d-6c4f-4e4a-97ac-db6e1790d144&width=2000&userId=&cache=v2",
         "content": text
     }
     requests.post(WEBHOOK_URL, json=data)
