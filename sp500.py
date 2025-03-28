53998f1aa252b8c7e5575e3d4dfb813102bb5d6b
yst314 <38710830+yst314@users.noreply.github.com>
2025-03-29 08:52:28 +0900

    Fix: 一昨日の終値とってたのを直した

--- sp500.py
+++ sp500.py
@@ -1,32 +1,18 @@
 import requests
 import datetime
 import os
-
-# Webhook URL
+import yfinance
 WEBHOOK_URL = os.environ["DISCORD_WEBHOOK_URL"]
 
-# S&P500のデータをYahoo Financeから取得
 def get_sp500_close():
-    url = "https://query1.finance.yahoo.com/v8/finance/chart/^GSPC"
-    params = {
-        "range": "2d",
-        "interval": "1d"
-    }
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-    res = requests.get(url, params=params, headers=headers)
+    ticker = "^GSPC"
+    data = yf.download(ticker, period="1d")
+    
+    if not data.empty:
+        last_price = data["Close"][ticker].iloc[0]
+    else:
+        raise ValueError("No data available for the specified ticker.")
 
-    if res.status_code != 200:
-        raise Exception(f"Yahoo Finance API error: {res.status_code}")
-
-    try:
-        closes = res.json()["chart"]["result"][0]["indicators"]["quote"][0]["close"]
-        last_close = closes[-2]  # 前日終値（-1は今日）
-        return round(last_close, 2)
-    except Exception as e:
-        print("レスポンス内容:", res.text)
-        raise e
 def send_to_discord(price):
     text = f"📈 S&P500 前日終値: ${price}"
     data = {
