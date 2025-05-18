c0f40df5fa5acaf43303af4d494bcf897c7012d1
Yoshito Osanai <oboroasamaru@gmail.com>
2025-05-19 08:26:20 +0900

    fix 日付計算方法のとprompt修正

--- common_utils.py
+++ common_utils.py
@@ -3,5 +3,5 @@ import datetime
 
 def get_calculated_date() -> str:
     """情報を取得したい日付（通常は前日）を計算し、YYYY年MM月DD日形式で返す関数。"""
-    date_to_fetch = datetime.datetime.now() - datetime.timedelta(days=1)
+    date_to_fetch = datetime.datetime.now()
     return date_to_fetch.strftime("%Y年%m月%d日")
